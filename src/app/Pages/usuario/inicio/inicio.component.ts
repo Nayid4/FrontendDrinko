ded5157753b8a0d6b4bb7911d0af86a98750d7c3
Nayid4 <nayid2004@gmail.com>
2024-06-07 02:01:31 -0500

    Ajuste de diseño

--- src/app/Pages/usuario/inicio/inicio.component.ts
+++ src/app/Pages/usuario/inicio/inicio.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { EncabezadoComponent } from '../../../shared/components/encabezado/encabezado.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CommonModule } from '@angular/common';
+import { TarjetaDeProductoComponent } from '../../../shared/components/tarjeta-de-producto/tarjeta-de-producto.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [EncabezadoComponent],
+  imports: [
+    EncabezadoComponent, 
+    SlickCarouselModule, 
+    CommonModule,
+    TarjetaDeProductoComponent
+  ],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  images = ['image1.jpg', 'image2.jpg', 'image3.jpg']; // Ruta de las imágenes
 
+  slickCarouselConfig = {
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 }
