0a529442459b365f5e6095e1717bfe020c99a5e0
Nayid4 <nayid2004@gmail.com>
2024-06-13 21:43:55 -0500

    Ajuste del problema de las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: 'usuario/inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: 'inicio', // Redirigir '/inicio' a '/usuario/inicio'
+        redirectTo: 'usuario/inicio',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
         loadChildren: () => import('./Pages/auth/auth.routes').then(m => m.AUTH_ROUTES)
     },
     {
@@ -17,5 +27,9 @@ export const routes: Routes = [
         path: 'inicio', // Redirigir '/inicio' a '/usuario/inicio'
         redirectTo: 'usuario/inicio',
         pathMatch: 'full'
+    },
+    {
+        path: '**', // Ruta para manejar cualquier otra ruta no definida
+        redirectTo: 'usuario/inicio'
     }
 ];
