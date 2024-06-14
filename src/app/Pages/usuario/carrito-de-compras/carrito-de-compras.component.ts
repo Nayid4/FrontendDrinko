54b418118b2ef0c6bd86e8cdfde7f96e4946b2ed
Nayid4 <nayid2004@gmail.com>
2024-06-13 23:20:42 -0500

    Agregacion de funcionalidad del carrito de compras

--- src/app/Pages/usuario/carrito-de-compras/carrito-de-compras.component.ts
+++ src/app/Pages/usuario/carrito-de-compras/carrito-de-compras.component.ts
@@ -1,15 +1,66 @@
-import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 import { EncabezadoComponent } from '../../../shared/components/encabezado/encabezado.component';
+import { TarjetaDeProductoDeCarritoComponent } from '../../../shared/components/tarjeta-de-producto-de-carrito/tarjeta-de-producto-de-carrito.component';
+import { AuthService } from '../../../core/services/auth.service';
+import { Carrito, ProductoCarrito } from '../../../core/models/carrito.model';
+import { AuthResponse } from '../../../core/models/auth.model';
+import { CarritoService } from '../../../core/services/carrito.service';
 
 @Component({
   selector: 'app-carrito-de-compras',
   standalone: true,
-  imports: [EncabezadoComponent],
+  imports: [EncabezadoComponent, TarjetaDeProductoDeCarritoComponent],
   templateUrl: './carrito-de-compras.component.html',
-  styleUrl: './carrito-de-compras.component.css'
+  styleUrls: ['./carrito-de-compras.component.css']
 })
-export class CarritoDeComprasComponent {
+export class CarritoDeComprasComponent implements OnInit {
 
+  productosCarrito: ProductoCarrito[] = []
+  usuario: AuthResponse | null = {} as AuthResponse
+  carrito: Carrito = {} as Carrito
+  total: number = 0
+
+  constructor(private authService: AuthService, private carritoService: CarritoService){}
+
+  ngOnInit(): void {
+    this.usuario = this.authService.obtenerDatosUsuario()
+    if (this.usuario == null){
+      return;
+    }
+
+    this.cargarProductosCarrito()
+  }
+
+  cargarProductosCarrito() : void{
+    this.carritoService.obtenerCarritoPorIdDeUsuario(this.usuario!.usuarioId).subscribe(
+      (carrito) => {
+        this.carrito = carrito
+        this.productosCarrito = carrito.productos
+        this.actualizarTotal()
+      },
+      error => {
+        console.log("Se ha presentado un error al cargar los productos: ",error)
+      }
+    );
+  }
+
+  actualizarTotal(): void {
+    this.total = this.productosCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0)
+  }
+
+  vaciarCarrito(): void {
+    // Implementar lógica para vaciar el carrito
+  }
+
+  comprar(): void {
+    // Implementar lógica para proceder con la compra
+  }
+
+  onProductoEliminado(): void {
+    this.cargarProductosCarrito();
+  }
+
+  onCantidadActualizada(): void {
+    this.actualizarTotal();
+  }
 }
